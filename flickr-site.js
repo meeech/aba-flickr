cd689075587449b137003a75df8eec49a95478df
Mitchell Amihod <mitchell.amihod@gmail.com>
2010-03-04 23:30:43 -0500

    some position.

--- flickr-site.js
+++ flickr-site.js
@@ -111,7 +111,6 @@ Y.delegate('click', function(e) {
     var tagName = this.get('title') || this.get('innerHTML');
     var gallery = bb[tagName]['gallery'] || buildGallery(tagName);
 
-
     //Maybe have it position itself below the thumbs, and just dont bother with CLOSE button - clicking on index thumbs will close one gal, open the other. 
     var overlay = new Y.Overlay({
         headerContent: tagName,
